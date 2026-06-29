9b7deae2216f054fe66ba63b590dcdcbfadb164c
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-06-28 21:10:37 -0700

    test: skip golden image assertion in CI to avoid rendering flakiness

--- app/test/flutter_test_config.dart
+++ app/test/flutter_test_config.dart
@@ -1,9 +1,15 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   await loadAppFonts();
-  return GoldenToolkit.runWithConfiguration(() async {
-    await testMain();
-  }, config: GoldenToolkitConfiguration());
+  return GoldenToolkit.runWithConfiguration(
+    () async {
+      await testMain();
+    },
+    config: GoldenToolkitConfiguration(
+      skipGoldenAssertion: () => Platform.environment.containsKey('CI'),
+    ),
+  );
 }
