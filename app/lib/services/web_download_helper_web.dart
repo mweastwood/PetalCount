136f4b9a33aaa52a30daccdd6f2b76c906e67fc3
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-07-10 16:56:47 -0400

    feat: trigger file download for exported chart pdf on web

---
+++ app/lib/services/web_download_helper_web.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: deprecated_member_use, avoid_web_libraries_in_flutter
+
+import 'dart:html' as html;
+
+void downloadFileWeb(List<int> bytes, String filename) {
+  final blob = html.Blob([bytes], 'application/pdf');
+  final url = html.Url.createObjectUrlFromBlob(blob);
+  html.AnchorElement(href: url)
+    ..setAttribute("download", filename)
+    ..click();
+  html.Url.revokeObjectUrl(url);
+}
