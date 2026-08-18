99edddcc9517c82c3eb92604f1b35e8e9d06bf96
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-08-18 07:32:38 +0000

    fix(android): enable core library desugaring and multidex
    Enable core library desugaring and multidex in app/android/app/build.gradle.kts to resolve Android release build failures caused by flutter_local_notifications requiring Java 8+ API desugaring.
    
    Closes #93

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -26,6 +27,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     flavorDimensions += "env"
@@ -87,4 +89,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
